a6444076b50450af171c6887ab88439f0af53f5d
cupschi <111538566+cupschi@users.noreply.github.com>
2026-09-16 07:03:31 -0500

    - Mecanum Drive Class - Mecanum teleop drive method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrive.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDrive {
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+
+    public void init(HardwareMap hwMap) {
+        frontLeftMotor = hwMap.get(DcMotor.class, "front_left_motor");
+        backLeftMotor = hwMap.get(DcMotor.class, "back_left_motor");
+        frontRightMotor = hwMap.get(DcMotor.class, "front_right_motor");
+        backRightMotor = hwMap.get(DcMotor.class, "back_right_motor");
+
+        //fixes mirroring so wheels spin the same direction
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
